fac71d94113b24ffdcc14179edd83e71bea2e280
Dwight Chan <cdhhao@126.com>
2023-10-15 16:29:28 +0800

    Update PartialTask2.swift
    task 取消顺序；async await 的作用

--- code/chapter-06_task/chapter-06_task/PartialTask2.swift
+++ code/chapter-06_task/chapter-06_task/PartialTask2.swift
@@ -5,6 +5,12 @@
 //  Created by 丰源天下传媒 on 2023/2/25.
 //
 
+/// 如果有多个 task ，task 1， task2
+/// 在 task2 中 调用 task1.cancel 方法
+/// 则 task1 先遍历 子任务，直到 最后一层的子任务，
+/// 然后 一层层， 从最后一层 先取消， 一层层取消（或者抛出异常），直到最后取消 task1 任务；
+
+
 import Foundation
   
  
